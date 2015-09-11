0db8fe2f91a9608f29ae81f68e20d3b3c969319a
hzx <zx_he@Ctrip.com>
2015-09-11 14:33:22 +0800

    增加AssemblyDescription

--- Expression2Sql/Properties/AssemblyInfo.cs
+++ Expression2Sql/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Expression2Sql")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Expression2Sql是一个可以将Expression表达式树解析成Transact-SQL的开源项目")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("StrangeCity")]
 [assembly: AssemblyProduct("Expression2Sql")]
